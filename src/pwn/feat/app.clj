157c9cded45e4431e10d680365baac7e8740c210
loganrios <loganrios1@gmail.com>
2022-11-18 22:47:05 -0700

    fix: cleanup/modularizing app page

--- src/pwn/feat/app.clj
+++ src/pwn/feat/app.clj
@@ -7,61 +7,84 @@
             [ring.adapter.jetty9 :as jetty]
             [cheshire.core :as cheshire]))
 
+(defn auth-info [email]
+  [:div "Signed in as " email ". "
+   (biff/form
+    {:action "/auth/signout"
+     :class "inline"}
+    [:button.text-blue-500.hover:text-blue-800 {:type "submit"}
+     "Sign out."])])
+
+(defn author-info [author]
+  [:div (str "Your pen name is: " (:author/pen-name author))
+   [:.text-sm "Author UUID: " (:xt/id author)]])
+
+(defn new-work-form []
+  (biff/form
+   {:action "/app/work"}
+   [:div "Create a new work"]
+   [:input#title
+    {:name "title"
+     :type "text"
+     :placeholder "Work title"}]
+   [:button.btn {:type "submit"} "Create"]))
+
+(defn uid->author [db user-id]
+  (-> (q db
+         '{:find [(pull author [*])]
+           :where [[author :author/user uid]]
+           :in [uid]}
+         user-id)
+      (ffirst)))
+
+(defn become-author-form []
+  (biff/form
+   {:action "/app/author"}
+   [:div "Become an author"]
+   [:input#pen-name
+    {:name "pen-name"
+     :type "text"
+     :placeholder "Pen Name"}]
+   [:button.btn {:type "submit"} "Create"]))
+
+(defn uid->works [db user-id]
+  (q db '{:find [(pull works [*])]
+          :where [[works :work/owner uid]]
+          :in [uid]}
+     user-id))
+
+(defn works-list [works]
+  (println works)
+  (if (seq works)
+    [:div
+     [:.h-3]
+     [:div "Works:"
+      (for [work works]
+        (let [work-map (first work)]
+         [:div
+          (:work/title work-map)
+          (biff/form
+           {:action (str "/app/work/" (:xt/id work-map) "/delete")
+            :class "inline"}
+           [:button.text-blue-500.hover:text-blue-800 {:type "submit"} "Delete"])]))]]
+    [:div "You have no works."]))
+
 (defn app [{:keys [session biff/db] :as req}]
-  (let [{:user/keys [email]} (xt/entity db (:uid session))]
+  (let [user-id (:uid session)
+        {:user/keys [email]} (xt/entity db user-id)]
     (ui/page
      {}
      nil
-     [:div "Signed in as " email ". "
-      (biff/form
-       {:action "/auth/signout"
-        :class "inline"}
-       [:button.text-blue-500.hover:text-blue-800 {:type "submit"}
-        "Sign out"])
-      "."]
+     (auth-info email)
      [:.h-3]
-     (if-some [author (ffirst
-                       (q db
-                          '{:find [(pull author [*])]
-                            :where [[author :author/user user-id]]
-                            :in [user-id]}
-                          (:uid session)))]
+     (if-some [author (uid->author db user-id)]
        [:div
-        [:div "Your author is: " (:author/pen-name author)]
-        [:div.text-sm "Author UUID: " (:xt/id author)]
+        (author-info author)
         [:.h-3]
-        (biff/form
-         {:action "/app/work"}
-         [:div "Create a new work"]
-         [:input#title
-          {:name "title"
-           :type "text"
-           :placeholder "Work title"}]
-         [:button.btn {:type "submit"} "Create"])
-        (let [works (q db
-                       '{:find [(pull works [*])]
-                         :where [[works :work/owner user-id]]
-                         :in [user-id]}
-                       (:uid session))]
-          (if (seq works)
-            [:div
-             [:.h-3]
-             [:div "Works:"
-              (for [work works]
-                [:div
-                 (:work/title (first work))
-                 (biff/form
-                  {:action (str "/app/work/" (:xt/id (first work)) "/delete")}
-                  [:button.btn {:type "submit"} "Delete"])])]]
-            [:div "You have no works."]))]
-       (biff/form
-        {:action "/app/author"}
-        [:div "Become an author"]
-        [:input#pen-name
-         {:name "pen-name"
-          :type "text"
-          :placeholder "Pen Name"}]
-        [:button.btn {:type "submit"} "Create"])))))
+        (new-work-form)
+        (let [works (uid->works db user-id)]
+          (works-list works))]
+       (become-author-form)))))
 
 (defn new-author [{:keys [params session] :as req}]
   (let [author-id (random-uuid)]
@@ -92,13 +115,13 @@
 
 (defn wrap-work [handler]
   (fn [{:keys [biff/db session path-params] :as req}]
-   (if-some [work (xt/entity db (parse-uuid (:id path-params)))]
-     (let [owner (:work/owner work)
-           user (:uid session)]
-       (if (= owner user)
-         (handler (assoc req :work work :owner owner))
-         (handler req)))
-     (handler req))))
+    (if-some [work (xt/entity db (parse-uuid (:id path-params)))]
+      (let [owner (:work/owner work)
+            user (:uid session)]
+        (if (= owner user)
+          (handler (assoc req :work work :owner owner))
+          (handler req)))
+      (handler req))))
 
 (def features
   {:routes ["/app" {:middleware [mid/wrap-signed-in]}
