710b7562df09977c7a16e651a0989ab3fd446d28
kyleforthman <kyle.forthman@yahoo.com>
2022-12-12 12:49:56 -0700

    feat: authors can add and update genres for works

--- src/pwn/repl.clj
+++ src/pwn/repl.clj
@@ -23,12 +23,33 @@
     (q db '{:find [e]
             :where [[e :author/user]]}))
 
+  ;;Genre Seed for Testing
+
+  (defn seed [documents]
+    (let [{:keys [biff/db] :as sys} (get-sys)]
+      (biff/submit-tx sys documents)))
+
+  (def genres ["Fantasy" "SciFi" "Romance"
+                 "Drama" "Contemporary" "Nonfiction"])
+
+  (defn gen-genre [nm]
+    {:db/doc-type :genre
+     :xt/id (keyword nm)
+     :genre/slug nm
+     :genre/description (str "This genre is " nm)
+     :genre/display-name nm})
+
+  (def genres-seed
+    (map gen-genre genres))
+
+  (seed genres-seed)
+
   (sort (keys @biff/system))
 
   (fq (:biff/db (get-sys)))
 
-  ;; Check the terminal for output.
+    ;; Check the terminal for output.
   (biff/submit-job (get-sys) :echo {:foo "bar"})
   (deref (biff/submit-job-for-result (get-sys) :echo {:foo "bar"}))
 
- nil)
+  nil)
