6020745d1b98a5c8e735f1f816afdcae15e13df4
loganrios <logan@keionsoft.com>
2023-03-31 23:05:41 -0600

    Possibly enable PWA

--- src/pwn/ui.clj
+++ src/pwn/ui.clj
@@ -19,15 +19,17 @@
                      :lang "en-US"
                      :icon "/img/pwn-square.png"
                      :description "The author-first platform for online fiction."})
-       (update :base/head (fn [head]
-                            (concat [[:link {:rel "stylesheet" :href (css-path)}]
-                                     [:link {:rel "manifest" :href "manifest.json"}]
-                                     [:script {:src "https://unpkg.com/htmx.org@1.8.6"}]
-                                     [:script {:src "https://unpkg.com/hyperscript.org@0.9.8"}]
-                                     (when recaptcha
-                                       [:script {:src "https://www.google.com/recaptcha/api.js"
-                                                 :async "async" :defer "defer"}])]
-                                    head))))
+       (update :base/head
+               (fn [head]
+                 (concat [[:link {:rel "stylesheet" :href (css-path)}]
+                          [:link {:rel "manifest" :href "manifest.json"}]
+                          [:script {:src "https://unpkg.com/htmx.org@1.8.6"}]
+                          [:script {:src "https://unpkg.com/hyperscript.org@0.9.8"}]
+                          [:script (biff/unsafe (slurp "resources/register-worker.js"))]
+                          (when recaptcha
+                            [:script {:src "https://www.google.com/recaptcha/api.js"
+                                      :async "async" :defer "defer"}])]
+                         head))))
    body))
 
 (defn header [{:keys [session]}]
